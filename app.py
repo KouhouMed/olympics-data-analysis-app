ec7df1cdffe9ffa6d4a961299582abbfe24f55f4
KouhouMed <mohamedkouhou@gmail.com>
2024-08-13 16:40:37 +0200

    top statistics

--- app.py
+++ app.py
@@ -93,3 +93,7 @@ if user_menu == 'Overall Analysis':
     sport_list = df['Sport'].unique().tolist()
     sport_list.sort()
     sport_list.insert(0,'Overall')
+
+    selected_sport = st.selectbox('Select a Sport',sport_list)
+    x = helper.most_successful(df,selected_sport)
+    st.table(x)
